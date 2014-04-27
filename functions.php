16226b2e48c4cd43356e27a4a7c2cbd4363fc3b6
Michael Fisher <michael@thefisherhome.net>
2014-04-27 14:20:49 -0500

    Added helper text to Featured Image meta box to specify the image should be at least 1040px wide.

--- functions.php
+++ functions.php
@@ -6,4 +6,10 @@ function child_after_setup_theme() {
 	set_post_thumbnail_size( 1040, 270, true );
 }
 
+add_action( 'do_meta_boxes', 'change_image_box', 11 );
+
+function change_image_box() {
+	remove_meta_box('postimagediv', __('Featured Image'), 'post_thumbnail_meta_box', null, 'side', 'low');
+	add_meta_box('postimagediv', __('Featured Image (min. 1040px wide)'), 'post_thumbnail_meta_box', null, 'side', 'low');
+}
 ?>
