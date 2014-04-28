2e530a96cc3278f75bd6be73c9f223ff3ff6a4b1
Michael Fisher <michael@thefisherhome.net>
2014-04-28 08:09:46 -0500

    Arrangement/alignment of post content elements.

--- functions.php
+++ functions.php
@@ -1,15 +1,16 @@
 <?php
 
-add_action( 'after_setup_theme', 'child_after_setup_theme', 11 );
-
 function child_after_setup_theme() {
 	set_post_thumbnail_size( 1040, 270, true );
 }
 
-add_action( 'do_meta_boxes', 'change_image_box', 11 );
+add_action( 'after_setup_theme', 'child_after_setup_theme', 11 );
 
 function change_image_box() {
 	remove_meta_box('postimagediv', __('Featured Image'), 'post_thumbnail_meta_box', null, 'side', 'low');
 	add_meta_box('postimagediv', __('Featured Image (min. 1040px wide)'), 'post_thumbnail_meta_box', null, 'side', 'low');
 }
+
+add_action( 'do_meta_boxes', 'change_image_box', 11 );
+
 ?>
