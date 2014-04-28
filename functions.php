9c26b59dd31ce5c15fbd6ca7f352f2da850b36f5
Michael Fisher <michael@thefisherhome.net>
2014-04-28 15:22:27 -0500

    Body font to 'Crimson'.

--- functions.php
+++ functions.php
@@ -6,9 +6,16 @@ function child_after_setup_theme() {
 
 add_action( 'after_setup_theme', 'child_after_setup_theme', 11 );
 
+function remove_google_fonts() {
+	wp_dequeue_style( 'twentythirteen-fonts' );
+}
+
+add_action( 'wp_enqueue_scripts', 'remove_google_fonts', 11 );
+
 function change_image_box() {
-	remove_meta_box('postimagediv', __('Featured Image'), 'post_thumbnail_meta_box', null, 'side', 'low');
-	add_meta_box('postimagediv', __('Featured Image (min. 1040px wide)'), 'post_thumbnail_meta_box', null, 'side', 'low');
+	if ( $thumbnail_support )
+		remove_meta_box('postimagediv', __('Featured Image'), 'post_thumbnail_meta_box', null, 'side', 'low');
+		add_meta_box('postimagediv', __('Featured Image (min. 1040px wide)'), 'post_thumbnail_meta_box', null, 'side', 'low');
 }
 
 add_action( 'do_meta_boxes', 'change_image_box', 11 );
