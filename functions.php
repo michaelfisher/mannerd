59455bdda66cc97eb099c8fa56d86d597aa6e1c7
Michael Fisher <michael@thefisherhome.net>
2014-04-29 20:03:03 -0500

    Some font changes. Major logo change. Footer date is now Roman numerals.

--- functions.php
+++ functions.php
@@ -20,4 +20,48 @@ function change_image_box() {
 
 add_action( 'do_meta_boxes', 'change_image_box', 11 );
 
+function twentythirteen_entry_meta() {
+	if ( is_sticky() && is_home() && ! is_paged() )
+		echo '<span class="featured-post">' . __( 'Sticky', 'twentythirteen' ) . '</span>';
+
+	if ( ! has_post_format( 'link' ) && 'post' == get_post_type() )
+		twentythirteen_entry_date();
+
+	// Translators: used between list items, there is a space after the comma.
+	$categories_list = get_the_category_list( __( ', ', 'twentythirteen' ) );
+	if ( $categories_list ) {
+		//echo '<span class="categories-links">' . $categories_list . '</span>';
+	}
+
+	// Translators: used between list items, there is a space after the comma.
+	$tag_list = get_the_tag_list( '', __( ', ', 'twentythirteen' ) );
+	if ( $tag_list ) {
+		echo '<span class="tags-links">' . $tag_list . '</span>';
+	}
+
+	// Post author
+	if ( 'post' == get_post_type() ) {
+		printf( '<span class="author vcard">Posted by: <a class="url fn n" href="%1$s" title="%2$s" rel="author">%3$s</a></span>',
+			esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ),
+			esc_attr( sprintf( __( 'View all posts by %s', 'twentythirteen' ), get_the_author() ) ),
+			get_the_author()
+		);
+	}
+}
+
+function number_to_roman($num) {
+	$n = intval($num);
+	$result = "";
+	$lookup = array('M' => 1000, 'CM' => 900, 'D' => 500, 'CD' => 400, 'C' => 100, 'XC' => 90, 'L' => 50, 'XL' => 40, 'X' => 10, 'IX' => 9, 'V' => 5, 'IV' => 4, 'I' => 1);
+
+	foreach ($lookup as $roman => $value) {
+		$matches = intval($n / $value);
+		$result .= str_repeat($roman, $matches);
+		$n = $n % $value;
+	}
+
+return $result;
+
+}
+
 ?>
