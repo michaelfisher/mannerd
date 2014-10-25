a6a6e4f5ad02fb046c027f91d2e96020c14af950
Michael Fisher <michael@thefisherhome.net>
2014-10-25 07:11:00 -0500

    Hiding social icons in footer, until we have content to be shared therein.

--- wp-content/themes/mannerd/footer.php
+++ wp-content/themes/mannerd/footer.php
@@ -16,9 +16,11 @@
 
 			<div class="site-info">
 				<?php do_action( 'twentythirteen_credits' ); ?>
-				<div id="social-icons">
-					<a id="facebook" target="_blank" href="http://facebook.com/mannerdisms"><i class="fa fa-facebook fa-fw fa-3x"></i></a><a id="twitter" target="_blank" href="http://twitter.com/mannerdisms"><i class="fa fa-twitter fa-fw fa-3x"></i></a><a id="pinterest" target="_blank" href="http://pinterest.com/mannerdisms"><i class="fa fa-pinterest fa-fw fa-3x"></i></a>
-				</div>
+				<!--<div id="social-icons">
+					<a id="facebook" target="_blank" href="http://facebook.com/mannerdisms"><i class="fa fa-facebook fa-fw fa-3x"></i></a>
+					<a id="twitter" target="_blank" href="http://twitter.com/mannerdisms"><i class="fa fa-twitter fa-fw fa-3x"></i></a>
+					<a id="pinterest" target="_blank" href="http://pinterest.com/mannerdisms"><i class="fa fa-pinterest fa-fw fa-3x"></i></a>
+				</div>-->
 				<a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><img src="<?php echo get_stylesheet_directory_uri() ?>/images/mannerd-white.png" title="<?php bloginfo( 'name' ); ?>" height="20px"/></a></br>
 				<a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">&copy; <?php echo number_to_roman(date('Y')); ?></a>
 			</div><!-- .site-info -->
