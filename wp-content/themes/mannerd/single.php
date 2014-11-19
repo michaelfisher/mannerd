0f9a7d141aa03c4a8f74d348e5ba17597741b707
Michael Fisher <michael@thefisherhome.net>
2014-11-19 17:07:16 -0600

    Added FB Like section beneath single pages.

---
+++ wp-content/themes/mannerd/single.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * The template for displaying all single posts
+ *
+ * @package WordPress
+ * @subpackage Twenty_Thirteen
+ * @since Twenty Thirteen 1.0
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<div id="content" class="site-content" role="main">
+
+			<?php /* The loop */ ?>
+			<?php while ( have_posts() ) : the_post(); ?>
+
+				<?php get_template_part( 'content', get_post_format() ); ?>
+				<div class="fb-like fb-like-single" data-href="https://www.facebook.com/mannerdisms" data-layout="standard" data-action="like" data-width="350px" data-show-faces="true" data-share="false"></div>
+				<div class="fb-like-caption">Like <a class="fb-like-single-link" href="https://www.facebook.com/mannerdisms">Mannerd</a> on Facebook!</div>
+				<?php twentythirteen_post_nav(); ?>
+				<?php comments_template(); ?>
+
+			<?php endwhile; ?>
+
+		</div><!-- #content -->
+	</div><!-- #primary -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
